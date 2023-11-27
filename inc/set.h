75e83681f944f254c076f7e5d39adb85a47db15f
lfelipediniz <lfediniz@gmail.com>
2023-11-27 11:19:37 -0300

    add functions

--- inc/set.h
+++ inc/set.h
@@ -2,9 +2,24 @@
 #define SET_H
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+typedef struct set_ SET;
+
+// basic operations
+SET *create_set();
+bool destroy_set(SET *sp);
+bool addElement_set(SET *sp, int ele);
+bool removeElement_set(SET *sp, int ele);
+void print_set(SET *sp);
+
+// specific operations
+bool isMember_set(SET *sp, int ele);
+SET *union_set(SET *sp1, SET *sp2);
+SET *intersection_set(SET *sp1, SET *sp2);
 
 
 #endif
